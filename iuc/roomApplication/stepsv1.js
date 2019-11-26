cb36e0a4aa56a58a5d4bb64779a6a34b2163eef1
sh963852741 <sh963852741@126.com>
2019-11-26 23:57:57 +0800

    修改步骤信息显示错误的bug

--- iuc/roomApplication/stepsv1.js
+++ iuc/roomApplication/stepsv1.js
@@ -1,26 +1,26 @@
 const steps = [{
 		id: 0,
 		name: '填写申请表',
-		finalState: "",
-		err: ""
+		color: "",
+		icon: ""
 	},
 	{
 		id: 1,
 		name: '指导老师审核',
-		finalState: "",
-		err: ""
+		color: "",
+		icon: ""
 	},
 	{
 		id: 2,
 		name: '主管审核',
-		finalState: "",
-		err: ""
+		color: "",
+		icon: ""
 	},
 	{
 		id: 3,
 		name: '管理员确认',
-		finalState: "",
-		err: ""
+		color: "",
+		icon: ""
 	}
 ]
 
