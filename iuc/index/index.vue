d1e2422942fbae618d4505b63b68c58657ab708e
liangyingshao <1669807584@qq.com>
2019-11-27 23:28:36 +0800

    index页面增加链接

--- iuc/index/index.vue
+++ iuc/index/index.vue
@@ -21,36 +21,36 @@
 				<view class="action">
 					<text class="text-bold">我的</text>
 				</view>
-				<view class="action more">收起<text class="cuIcon-playfill"></text></view>
+				<view class="action more" @click="mineClick()">{{mineShow ? "收起" : "展开"}}<text class="cuIcon-playfill"></text></view>
 			</view>
-			<view class="cu-list grid col-4 no-border">
-				<view class="cu-item" v-for='(item,index) in mine' :key='index'>
+			<transition-group class="cu-list grid col-4 no-border" v-show="mineShow">
+				<view class="cu-item" v-for='(item,index) in mine' :key='index' @click="navTo(item.soure)">
 					<view>
 						<image :src="item.image" class="cu-avatar bg-white lg" mode="aspectFit"></image>
 					</view>
 					<text>{{item.text}}</text>
 				</view>
-			</view>
+			</transition-group>
 		</view>
 		<view>
 			<view class="cu-bar bg-white">
 				<view class="action">
 					<text class="text-bold">功能</text>
 				</view>
-				<view class="action more">收起<text class="cuIcon-playfill"></text></view>
+				<view class="action more" @click="functionClick">{{functionshow ? "收起" : "展开"}}<text class="cuIcon-playfill"></text></view>
 			</view>
-			<view class="cu-list grid col-4 no-border">
-				<view class="cu-item" v-for='(item,index) in functionList' :key='index'>
-					<view>
-						<image :src="item.image" class="cu-avatar bg-white lg" mode="aspectFit"></image>
+			<view class="cu-list grid col-4 no-border" v-show="functionshow">
+					<view class="cu-item" v-for='(item,index) in functionList' :key='index' @click="navTo(item.soure)">
+						<view>
+							<image :src="item.image" class="cu-avatar bg-white lg" mode="aspectFit"></image>
+						</view>
+						<text>{{item.text}}</text>
 					</view>
-					<text>{{item.text}}</text>
-				</view>
 			</view>
 		</view>
 		<view style="height: 100rpx;">
 			<view class="cu-bar tabbar bg-white foot">
-				<view v-for="(item,index) in tabInfo" class="action" :key="index">
+				<view v-for="(item,index) in tabInfo" class="action" :key="index" @click="navTo(item.nav)">
 					<view :class="{'text-blue':index === 0}">
 						<text :class="item.icon"></text>
 						<text class="text-lg">{{item.text}}</text>
@@ -73,65 +73,84 @@
 				},
 				mine: [{
 						image: "../../static/needToDo.png",
-						text: "我的代办"
+						text: "我的待办" ,
+						soure: "../roomApplication/v1/list"
 					},
 					{
 						image: "../../static/myJoint.png",
-						text: "我的参与"
+						text: "我的参与",
+						soure: ""
 					},
 					{
 						image: "../../static/myApplication.png",
-						text: "我的申请"
+						text: "我的申请",
+						soure: "../roomApplication/v1/list"
 					},
 					{
 						image: "../../static/labList.png",
-						text: "待开发"
+						text: "待开发",
+						soure: ""
 					},
 				],
 				functionList: [{
 						image: "../../static/labList.png",
-						text: "查看实验室"
+						text: "查看实验室",
+						soure: "../roomApplication/v1/labList"
 					},
 					{
 						image: "../../static/labAppointment.png",
-						text: "申请实验室"
+						text: "申请实验室",
+						soure: "../roomApplication/v1/create"
 					},
 					{
 						image: "../../static/labList.png",
-						text: "功能待开发"
+						text: "所有申请",
+						soure: "../roomApplication/v1/alllist"
 					},
 					{
 						image: "../../static/labList.png",
-						text: "功能待开发"
+						text: "功能待开发",
+						soure: ""
 					},
 				],
-				tabInfo:[
-					{
+				tabInfo: [{
 						icon: 'cuIcon-home',
 						text: '主页',
-						nav:''
+						nav: ''
 					},
 					{
 						icon: 'cuIcon-notice',
 						text: '通知公告',
-						nav:''
+						nav: ''
 					},
 					{
 						icon: 'cuIcon-notice',
 						text: '待开发',
-						nav:''
+						nav: ''
 					},
 					{
 						icon: 'cuIcon-my',
 						text: '我的',
-						nav:''
+						nav: '../profile/profile'
 					}
-				]
+				],
+				mineShow: true,
+				functionshow: true
+			}
+		},
+		methods: {
+			functionClick(){
+				this.functionshow = !this.functionshow;
+			},
+			mineClick() {
+				this.mineShow = !this.mineShow;
+			},
+			navTo(url) {
+				uni.navigateTo({
+					url: url
+				})
 			}
 		}
-	}
-	methods: {
-
 	}
 </script>
 
