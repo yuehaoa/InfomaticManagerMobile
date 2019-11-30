86e1dc389444557dc8ee196b69fdfa59628a7c7f
yuehaoa <53177683+yuehaoa@users.noreply.github.com>
2019-11-30 16:55:30 +0800

    修改实验室列表组件+RoomCode,修改alllist增加收起功能

--- components/labComponent/labInfoCard.vue
+++ components/labComponent/labInfoCard.vue
@@ -3,11 +3,10 @@
 		<view class="shadow shadow-lg bg-white bg-color">
 			<view class="flex padding-lr justify-between align-center padding-tb-sm">
 				<view class="logo" :style="{ backgroundImage: `url('${icon}')`}"></view>
-				<view class="text-xl text-white">{{`${lab.Building.SubCampus} ${lab.Building.Name}`}}</view>
+				<view class="text-xl text-white">{{lab.RoomCode}}</view>
 			</view>
 			<view class="text-black bg-gray padding-tb text-center">
-				<view><text class="text-bold text-xxl">{{lab.Name}}</text></view>
-				<view><text class="text-sm">Software Engineering Lab</text></view>
+				<text class="text-bold text-xxl">{{lab.Name}}</text>
 			</view>
 			<view class="flex justify-between text-white padding-lr padding-tb-xs text-center">
 				<view>{{`责任人: ${lab.Administrator} ${lab.AdminTelephone}`}}</view>
@@ -50,10 +49,6 @@
 		border-top-left-radius: 12%;
 		border-bottom-right-radius: 12%;
 	}
-	/*.title{
-		background-color:#E7EBED;
-		height:65px;
-	}*/
 	.logo{
 		width: 300rpx;
 		height: 60rpx;
