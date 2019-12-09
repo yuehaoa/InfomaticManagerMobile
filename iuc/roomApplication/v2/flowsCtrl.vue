93fd839a595ac58836c0019dc84c48300d2654ba
sh963852741 <sh963852741@126.com>
2019-12-09 22:31:08 +0800

    Create flowsCtrl.vue

---
+++ iuc/roomApplication/v2/flowsCtrl.vue
@@ -0,0 +1,12 @@
+<template>
+	<cu-custom bgColor="bg-informatic-brown" isBack>
+		<block slot="backText">返回</block>
+		<block slot="content">查看申请表</block>
+	</cu-custom>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
